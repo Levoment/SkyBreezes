f061836bc8d7b6bd458aecd4b10542b8bba9bda4
Levoment <30224138+Levoment@users.noreply.github.com>
2020-06-27 18:06:06 -0500

    Initial commit

---
+++ src/main/java/com/github/levoment/skybreezes/SkyBreezes.java
@@ -0,0 +1,10 @@
+package com.github.levoment.skybreezes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkyBreezes implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
